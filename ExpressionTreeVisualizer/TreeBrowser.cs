8ecc3b05cc58d1c571a8ee5a17cc689b9263843e
Stefan Henneken <stefan.henneken@live.com>
2019-01-03 11:14:23 +0100

    Add project files.

---
+++ ExpressionTreeVisualizer/TreeBrowser.cs
@@ -0,0 +1,16 @@
+namespace ExpressionTreeVisualizer
+{
+    using System.Windows.Forms;
+
+    public class TreeBrowser : TreeView
+    {
+        public TreeBrowser()
+        {
+        }
+
+        public void Add(TreeNode root)
+        {
+            Nodes.Add(root);
+        }
+    }
+}
